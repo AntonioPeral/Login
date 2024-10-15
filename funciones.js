1759d9dce02a2085b029026f48b198db5aa80b55
Antonio Salinas <antoniosalinas@MacBook-Air-de-Antonio.local>
2024-10-15 10:16:58 -0600

    Proyecto Login

---
+++ funciones.js
@@ -0,0 +1,6 @@
+document
+  .getElementById("loginForm")
+  .addEventListener("submit", function (event) {
+    // El evento de enviar el formulario será manejado por el navegador automáticamente
+    // Puedes agregar aquí lógica adicional si es necesario
+  });
